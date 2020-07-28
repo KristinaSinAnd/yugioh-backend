980763408f59dbe3b03c91894d628e070933c849
Vineta <vineta.gajevska@gmail.com>
2020-07-28 14:13:25 +0300

    created Enum CardCondition, updated info in Enum Rarity.

--- src/main/java/com/javariga6/yugioh/model/Rarity.java
+++ src/main/java/com/javariga6/yugioh/model/Rarity.java
@@ -2,6 +2,14 @@ package com.javariga6.yugioh.model;
 
 public enum Rarity {
 
-    SHINY,
-    NOTSHINY
+    COMMON,
+    RARE,
+    SUPER_RARE,
+    ULTRA_RARE,
+    ULTIMATE_RARE,
+    SECRET_RARE,
+    STARFOIL_RARE,
+    GOLD_RARE,
+    GHOST_RARE,
+    COLLECTORS_RARE
 }
