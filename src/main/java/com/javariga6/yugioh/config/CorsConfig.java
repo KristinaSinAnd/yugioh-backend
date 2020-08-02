251d30fdf7d60e40026c151f9f0565c15753fb20
Kristina <kristina.sinakovica@inbox.lv>
2020-08-02 11:38:58 +0300

    config

---
+++ src/main/java/com/javariga6/yugioh/config/CorsConfig.java
@@ -0,0 +1,19 @@
+package com.javariga6.yugioh.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class CorsConfig implements WebMvcConfigurer {
+    @Configuration
+    @EnableWebMvc
+    public class CorsConfig implements WebMvcConfigurer {
+
+        @Override
+        public void addCorsMappings(CorsRegistry registry) {
+            registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE");
+        }
+    }
+
+}
