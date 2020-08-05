d857d3dd77f5324808160001e67d4b18729bf4a4
Vineta <vineta.gajevska@gmail.com>
2020-08-05 16:32:06 +0300

    added path "/create" to CardStorageController for save method of CardStorage.

--- src/main/java/com/javariga6/yugioh/controller/CardStorageController.java
+++ src/main/java/com/javariga6/yugioh/controller/CardStorageController.java
@@ -16,6 +16,7 @@ public class CardStorageController {
     }
 
     @PostMapping
+    @RequestMapping("/create")
     public void save(@RequestBody CardStorage cardStorage) {
         cardStorageService.saveCardStorage(cardStorage);
     }
