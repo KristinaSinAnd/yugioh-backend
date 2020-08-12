100c38484e27ef79d652e7bd61ef4eb08726420e
Olegs <olegs.katajevs@gmail.com>
2020-08-12 18:38:24 +0300

    Role update part updated by Oleg (12.08.2020)

--- src/main/java/com/javariga6/yugioh/controller/RoleController.java
+++ src/main/java/com/javariga6/yugioh/controller/RoleController.java
@@ -2,6 +2,7 @@ package com.javariga6.yugioh.controller;
 
 import com.javariga6.yugioh.model.CardStorage;
 import com.javariga6.yugioh.model.Role;
+import com.javariga6.yugioh.model.User;
 import com.javariga6.yugioh.service.RoleService;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,22 +19,27 @@ public class RoleController {
 
     @PostMapping
     @RequestMapping("/create")
-    public void saveRole(@RequestBody Role role){
+    public void saveRole(@RequestBody Role role) {
         roleService.saveRole(role);
     }
 
     @GetMapping("/list")
-    public List<Role> getAllRoles(){
+    public List<Role> getAllRoles() {
         return roleService.getAll();
     }
 
     @GetMapping("/id/{id}")
-    public Role getRoleById(@PathVariable Long id){
+    public Role getRoleById(@PathVariable Long id) {
         return roleService.getRoleById(id);
     }
 
     @PostMapping("/delete")
     public void delete(@RequestBody Role role) {
         roleService.deleteRole(role);
+    }
+
+    @PostMapping("/update")
+    public void update(@RequestBody Role role) {
+        this.roleService.updateRole(role);
     }
 }
